af8189fb1450e7735f23fdf0913f750b8334ff7b
EnesKy <eneskamilyilmaz@gmail.com>
2020-10-02 11:49:12 +0300

    Login ekranı tasarlandı. Material ve Glide dependencyleri eklendi. Glide yapısı Module içine eklendi ve Activity'de kullanıldı.

--- app/src/main/java/com/enesky/daggerplayground/AuthActivity.kt
+++ app/src/main/java/com/enesky/daggerplayground/AuthActivity.kt
@@ -1,19 +1,28 @@
 package com.enesky.daggerplayground
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
+import com.bumptech.glide.RequestManager
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_auth.*
 import javax.inject.Inject
 
 class AuthActivity : DaggerAppCompatActivity() {
-    @Inject lateinit var dummyString: String
+
+    @Inject lateinit var logo: Drawable
+    @Inject lateinit var requestManager: RequestManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
-        Log.d(TAG, "onCreate - Dummy String = $dummyString")
+        setLogo()
 
+    }
+
+    private fun setLogo() {
+        requestManager.load(logo).into(iv_icon)
     }
 
     companion object {
--- app/src/main/java/com/enesky/daggerplayground/di/AppModule.kt
+++ app/src/main/java/com/enesky/daggerplayground/di/AppModule.kt
@@ -1,5 +1,12 @@
 package com.enesky.daggerplayground.di
 
+import android.app.Application
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.request.RequestOptions
+import com.enesky.daggerplayground.R
 import dagger.Module
 import dagger.Provides
 
@@ -10,7 +17,25 @@ import dagger.Provides
 @Module
 class AppModule {
 
-    @Provides
-    fun provideDummyString(): String = "lorem ipsum"
+    companion object {
+
+        @Provides
+        fun provideRequestOptions(): RequestOptions =
+            RequestOptions()
+                .placeholder(R.drawable.white_background)
+                .error(R.drawable.white_background)
+
+        @Provides
+        fun provideGlideInstance(application: Application,
+                                 requestOptions: RequestOptions): RequestManager =
+            Glide
+                .with(application)
+                .setDefaultRequestOptions(requestOptions)
+
+        @Provides
+        fun provideAppDrawable(application: Application): Drawable =
+            ContextCompat.getDrawable(application, R.drawable.decorative_colorful)!!
+
+    }
 
 }
